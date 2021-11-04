98a54b0efd3051a9a45c0b4a6d05302a77769559
Roberto Freire Ferraz Passos <roberto.passos@radixeng.com.br>
2021-11-04 16:11:33 -0300

    SINGLETON - PROBLEM

--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
+import { UserModel } from './app/models/user-model';
 import { UserService } from './app/services/users/user-service';
 
 const userService = new UserService();
+const userService2 = new UserService();
+
+userService.add(new UserModel("Antônio", 26));
 
 let users = userService.getAll();
+let users2 = userService2.getAll();
 
 console.log(users);
+console.log(users2);
