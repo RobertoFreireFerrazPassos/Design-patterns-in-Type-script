e173ed8ac87f6c7e45cd1b7a6e2bbbeab73e4e56
Roberto Freire Ferraz Passos <roberto.passos@radixeng.com.br>
2021-11-06 09:45:46 -0300

    SINGLETON - SOLUTION 2

--- app/services/users/user-service.ts
+++ app/services/users/user-service.ts
@@ -1,28 +1,18 @@
 import { UserModel } from '../../models/user-model';
 
-export class UserSingletonService {
-    static userService : UserService = null;
+export class UserService {
+    static instance : UserService;
 
-    // there is not static class in typescript, so the constructor must be private
-    private constructor(){
-    };
-
-    public static getInstance() {
-        if (this.userService == null) {
-            this.userService = new UserService();
-        } 
-        return this.userService;
-    }
-}
-
-// It is not exported
-class UserService {
     users : Array<UserModel> = [
         new UserModel("João", 23),
         new UserModel("Maria", 22)
     ];
 
     constructor(){
+        if (UserService.instance) {
+            return UserService.instance;
+        } 
+        UserService.instance = this;
     }
 
     public getAll() : Array<UserModel> {
--- index.ts
+++ index.ts
@@ -1,16 +1,13 @@
 import { UserModel } from './app/models/user-model';
-import { UserSingletonService } from './app/services/users/user-service';
+import { UserService } from './app/services/users/user-service';
 
 /* 
 Singleton is a creational design pattern that allows you to ensure 
 that a class has only one instance, while providing a global access point for that instance.
 */
 
-// Error: Constructor of class 'UserSingletonService' is private
-// const userService3 = new UserSingletonService();
-
-const userService = UserSingletonService.getInstance();
-const userService2 = UserSingletonService.getInstance();
+const userService = new UserService();
+const userService2 = new UserService();
 
 userService.add(new UserModel("Antônio", 26));
 
